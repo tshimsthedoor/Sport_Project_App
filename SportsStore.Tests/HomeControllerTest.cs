1818a85892bb8b534900ec94481219d5b5e4baa3
tresor <tshims79@gmail.com>
2023-06-04 17:02:31 +0200

    I have added the component for my view and I can navigate to my app only when there is an item

--- SportsStore.Tests/HomeControllerTest.cs
+++ SportsStore.Tests/HomeControllerTest.cs
@@ -30,7 +30,7 @@ public class HomeControllerTest
         HomeController controller = new(mock.Object);
 
         // Act
-        ProductsListViewModel result = controller.Index()?.ViewData.Model as ProductsListViewModel ?? new();
+        ProductsListViewModel result = controller.Index(null)?.ViewData.Model as ProductsListViewModel ?? new();
 
         // Assert
         Product[] prodArray = result.Products.ToArray();
@@ -78,7 +78,7 @@ public class HomeControllerTest
         controller.PageSize = 3;
 
         // Act
-        ProductsListViewModel result = controller.Index(2)?.ViewData.Model as ProductsListViewModel ?? new();
+        ProductsListViewModel result = controller.Index(null,2)?.ViewData.Model as ProductsListViewModel ?? new();
 
         // Assert
         Product[] prodArray = result.Products.ToArray();
@@ -104,7 +104,7 @@ public class HomeControllerTest
         new HomeController(mock.Object) { PageSize = 3 };
         // Act
         ProductsListViewModel result =
-        controller.Index(2)?.ViewData.Model as ProductsListViewModel
+        controller.Index(null,2)?.ViewData.Model as ProductsListViewModel
         ?? new();
         // Assert
         PagingInfo pageInfo = result.PagingInfo;
@@ -112,5 +112,57 @@ public class HomeControllerTest
         Assert.Equal(3, pageInfo.ItemsPerPage);
         Assert.Equal(5, pageInfo.TotalItems);
         Assert.Equal(2, pageInfo.TotalPages);
+    }
+
+    [Fact]
+    public void Can_Filter_Products()
+    {
+        // Arrange
+        // Create the mock repository
+        Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+        mock.Setup(m => m.Products).Returns((new Product[]
+        {
+            new Product
+            {
+                ProductID = 1,
+                Name = "P1",
+                Category = "Cat1"
+            },
+            new Product
+            {
+                ProductID = 2,
+                Name = "P2",
+                Category = "Cat2"
+            },
+            new Product
+            {
+                ProductID = 3,
+                Name = "P3",
+                Category = "Cat3"
+            },
+            new Product
+            {
+                ProductID = 4,
+                Name = "P4",
+                Category = "Cat4"
+            },
+            new Product
+            {
+                ProductID = 5,
+                Name = "P5",
+                Category = "Cat5"
+            }
+        }).AsQueryable<Product>());
+
+        // Arrange - create a controller and make the page size 3 items
+        HomeController controller = new HomeController(mock.Object);
+        controller.PageSize = 3;
+        // Action
+        Product[] result = (controller.Index("Cat2", 1)?.ViewData.Model
+        as ProductsListViewModel ?? new()).Products.ToArray();
+        // Assert
+        Assert.Equal(1, result.Length);
+        Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+        //Assert.True(result[1].Name == "P4" && result[1].Category == "Cat4");
     }
 }
