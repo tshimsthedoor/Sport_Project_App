484df7b8f24336b2fce8780165394ac6c2731934
tresor <tshims79@gmail.com>
2023-06-04 12:39:11 +0200

    added support for pagination so that the view displays a smaller number of products on a page and so the user can move from page to page to view the overall catalog

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -7,17 +7,19 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
 
-        public IActionResult Index()
-        {
-            return View(repository.Products);
-        }
+        public ViewResult Index(int productPage = 1)
+           => View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
 
-       
+
     }
 }
